2cfd86dbf1ce039c51da9ea195269e43adf73fd3
Akash Bhadani <akashbhadani5050@gmail.com>
2024-09-17 16:54:05 +0530

    Update score

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-okconst screens = document.querySelectorAll('.screen');
+const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 const start_btn = document.getElementById('start-btn')
 const game_container = document.getElementById('game-container')
