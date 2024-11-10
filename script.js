ca0617ae70e2e12085e8109382bcf952183b397a
Akash Bhadani <akashbhadani5050@gmail.com>
2024-11-10 22:22:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function addInsects() {
 
 function increaseScore() {
     score++
-    if(score > 10000) {
+    if(score > 50) {
         message.classList.add('visible')
     }
     scoreEl.innerHTML = `Score: ${score}`
